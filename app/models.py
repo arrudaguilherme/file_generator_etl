0f7321112a5d1baa1696b8116ce84973100485f9
Guilherme <guiga.arruda@hotmail.com>
2024-12-31 04:27:59 +0100

    added to generation: feedback, product categories, products, shipping

--- app/models.py
+++ app/models.py
@@ -2,49 +2,112 @@ from sqlalchemy import Column, String, DateTime, Integer, Boolean, Float, Foreig
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from database import Base
+from uuid import uuid4
 
 class UserModel(Base):
     __tablename__ = "users"
-    user_id = Column(Integer,primary_key=True, index= True)
-    name = Column(String,nullable=False) 
-    birth_date = Column(DateTime,nullable=False) 
-    gender = Column(String(1),nullable=False) 
-    phone_number = Column(String(),nullable=True) 
-    email = Column(String,nullable=False) 
-    address = Column(String,nullable=True) 
-    postal_code = Column(String(30),nullable=True) 
-    city = Column(String,nullable=True) 
-    state = Column(String,nullable=True) 
-    country = Column(String,nullable=True) 
-    profession = Column(String,nullable=True) 
-    company = Column(String,nullable=True) 
-    language = Column(String,nullable=True) 
-    marrital_status = Column(Boolean,nullable=False) 
-    date_generated = Column(DateTime,nullable=False)
-    created_at = Column(DateTime, default=func.now()) 
 
+    user_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    birth_date = Column(DateTime, nullable=False)
+    gender = Column(String(1), nullable=False)
+    phone_number = Column(String, nullable=True)
+    email = Column(String, nullable=False)
+    address = Column(String, nullable=True)
+    postal_code = Column(String(30), nullable=True)
+    city = Column(String, nullable=True)
+    state = Column(String, nullable=True)
+    country = Column(String, nullable=True)
+    profession = Column(String, nullable=True)
+    company = Column(String, nullable=True)
+    language = Column(String, nullable=True)
+    marrital_status = Column(Boolean, nullable=False)
+    date_generated = Column(DateTime, nullable=False)
+    created_at = Column(DateTime, default=func.now())
+
+    
     sales = relationship("SalesModel", back_populates="user")
+    feedbacks = relationship("FeedbackModel", back_populates="user")
+    campaigns = relationship("MarketingCampaignModel", back_populates="user")
 
 class SalesModel(Base):
     __tablename__ = "sales"
 
-    transaction_id = Column(String, primary_key=True, index=True) 
-    user_id = Column(String, ForeignKey("users.user_id"), nullable=False) 
-    product_id = Column(String, nullable=False) 
-    product_name = Column(String, nullable=False) 
-    category = Column(String, nullable=True) 
-    brand = Column(String, nullable=True) 
-    quantity = Column(Integer, nullable=False, default=1) 
-    price_per_unit = Column(Float, nullable=False) 
-    discount_percentage = Column(Float, nullable=True, default=0) 
-    discount_value = Column(Float, nullable=True, default=0) 
-    total_value = Column(Float, nullable=False) 
-    sale_date = Column(DateTime,nullable=False) 
-    payment_method = Column(String, nullable=False) 
-    store_location = Column(String, nullable=True) 
-    region = Column(String, nullable=True) 
-    shipping_cost = Column(Float, nullable=True) 
-    delivery_time_days = Column(Integer, nullable=True) 
-    customer_feedback = Column(String, nullable=True) 
+    transaction_id = Column(String, primary_key=True, index=True, default=lambda: str(uuid4()))
+    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False)
+    product_id = Column(String, nullable=False)
+    product_name = Column(String, nullable=False)
+    category = Column(String, nullable=True)
+    brand = Column(String, nullable=True)
+    quantity = Column(Integer, nullable=False, default=1)
+    price_per_unit = Column(Float, nullable=False)
+    discount_percentage = Column(Float, nullable=True, default=0)
+    discount_value = Column(Float, nullable=True, default=0)
+    total_value = Column(Float, nullable=False)
+    sale_date = Column(DateTime, nullable=False)
+    payment_method = Column(String, nullable=False)
+    store_location = Column(String, nullable=True)
+    region = Column(String, nullable=True)
+    shipping_cost = Column(Float, nullable=True)
+    delivery_time_days = Column(Integer, nullable=True)
+    customer_feedback = Column(String, nullable=True)
 
-    user = relationship("Usermodel", back_populates="sales")
+    
+    user = relationship("UserModel", back_populates="sales")
+    shipping = relationship("ShippingModel", back_populates="sale")
+    feedback = relationship("FeedbackModel", back_populates="sale")
+
+class ShippingModel(Base):
+    __tablename__ = "shipping"
+
+    shipping_id = Column(String, primary_key=True, index=True, default=lambda: str(uuid4()))
+    transaction_id = Column(String, ForeignKey("sales.transaction_id"), nullable=False)
+    carrier = Column(String, nullable=False)
+    shipping_method = Column(String, nullable=False)
+    shipping_cost = Column(Float, nullable=False)
+    delivery_time_days = Column(Integer, nullable=False)
+    delivery_status = Column(String, nullable=False)
+
+    sale = relationship("SalesModel", back_populates="shipping")
+
+class FeedbackModel(Base):
+    __tablename__ = "feedbacks"
+
+    feedback_id = Column(String, primary_key=True, index=True, default=lambda: str(uuid4()))
+    transaction_id = Column(String, ForeignKey("sales.transaction_id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False)
+    feedback_text = Column(String, nullable=True)
+    feedback_rating = Column(Integer, nullable=True)  # De 1 a 5, por exemplo.
+
+    user = relationship("UserModel", back_populates="feedbacks")
+    sale = relationship("SalesModel", back_populates="feedback")
+
+class MarketingCampaignModel(Base):
+    __tablename__ = "marketing_campaigns"
+
+    campaign_id = Column(String, primary_key=True, index=True, default=lambda: str(uuid4()))
+    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False)
+    campaign_name = Column(String, nullable=False)
+    campaign_date = Column(DateTime, default=func.now())
+    campaign_type = Column(String, nullable=False)
+
+    user = relationship("UserModel", back_populates="campaigns")
+
+class ProductModel(Base):
+    __tablename__ = "products"
+
+    product_id = Column(String, primary_key=True, index=True, default=lambda: str(uuid4()))
+    product_name = Column(String, nullable=False)
+    category_id = Column(String, ForeignKey("categories.category_id"), nullable=False)
+    price_per_unit = Column(Float, nullable=False)
+    brand = Column(String, nullable=True)
+
+    category = relationship("CategoryModel", back_populates="products")
+
+class CategoryModel(Base):
+    __tablename__ = "categories"
+
+    category_id = Column(String, primary_key=True, index=True, default=lambda: str(uuid4()))
+    category_name = Column(String, nullable=False)
+
+    products = relationship("ProductModel", back_populates="category")
--- producer/generator.py
+++ producer/generator.py
@@ -7,50 +7,34 @@ import time
 
 fake = Faker("en_US")
 
-def generate_sales_data():
+def generate_sales_data(user_ids):
     try:
-        user = {
-             "name": fake.name(),
-            "birth_date":fake.date_of_birth().isoformat(),
-            "gender":fake.random_element(["M","F"]),
-            "phone_number":fake.phone_number(),
-            "email":fake.email(),
-            "address":fake.address(),
-            "postal_code":fake.postcode(),
-            "city":fake.city(),
-            "state":fake.state(),
-            "country":fake.country(),
-            "profession":fake.job(),
-            "company":fake.company(),
-            "language":fake.language_name(),
-            "marrital_status":fake.boolean(),
-            "date_generated":datetime.now().strftime("%Y-%m-%d %H:%M:%S_%f")
-        }
         sale = {
-            "transaction_id": fake.uuid4(),  
-            "product_id": fake.uuid4(),  
-            "product_name": fake.word().capitalize(),  
-            "category": fake.random_element(["Electronics", "Clothing", "Books", "Home Appliances", "Toys"]),  
-            "brand": fake.company(),  
-            "quantity": random.randint(1, 20),  
-            "price_per_unit": round(random.uniform(10, 1000), 2),  
-            "discount_percentage": random.choice([0, 5, 10, 15, 20]),  
-            "discount_value": None,  
-            "total_value": None,  
-            "sale_date": fake.date_time_this_year().strftime("%Y-%m-%d %H:%M:%S"),  
-            "payment_method": fake.random_element(["Credit Card", "Debit Card", "Cash", "Online Payment", "Gift Card"]),  
-            "customer_type": fake.random_element(["New", "Returning", "VIP"]),  
-            "store_location": fake.city(),  
-            "region": fake.random_element(["North", "South", "East", "West", "Central"]),  
-            "sales_representative": fake.name(),  
-            "shipping_cost": round(random.uniform(5, 50), 2),  
-            "delivery_time_days": random.randint(1, 7),  
-            "customer_feedback": fake.random_element(["Positive", "Neutral", "Negative", "None"]),  
+            "transaction_id": fake.uuid4(),
+            "user_id": random.choice(user_ids),
+            "product_id": fake.uuid4(),
+            "product_name": fake.word().capitalize(),
+            "category": fake.random_element(["Electronics", "Clothing", "Books", "Home Appliances", "Toys"]),
+            "brand": fake.company(),
+            "quantity": random.randint(1, 20),
+            "price_per_unit": round(random.uniform(10, 1000), 2),
+            "discount_percentage": random.choice([0, 5, 10, 15, 20]),
+            "discount_value": None,
+            "total_value": None,
+            "sale_date": fake.date_time_this_year().strftime("%Y-%m-%d %H:%M:%S"),
+            "payment_method": fake.random_element(["Credit Card", "Debit Card", "Cash", "Online Payment", "Gift Card"]),
+            "customer_type": fake.random_element(["New", "Returning", "VIP"]),
+            "store_location": fake.city(),
+            "region": fake.random_element(["North", "South", "East", "West", "Central"]),
+            "sales_representative": fake.name(),
+            "shipping_cost": round(random.uniform(5, 50), 2),
+            "delivery_time_days": random.randint(1, 7),
+            "customer_feedback": fake.random_element(["Positive", "Neutral", "Negative", "None"]),
         }
-        
+
         sale["discount_value"] = round(sale["price_per_unit"] * (sale["discount_percentage"] / 100), 2)
         sale["total_value"] = round(sale["quantity"] * (sale["price_per_unit"] - sale["discount_value"]) + sale["shipping_cost"], 2)
-        
+
         return sale
     except Exception as e:
         print(f"Error generating sales data: {e}")
@@ -58,50 +42,174 @@ def generate_sales_data():
 def generate_user_data():
     try:
         user = {
+            "user_id": fake.uuid4(),  
             "name": fake.name(),
-            "birth_date":fake.date_of_birth().isoformat(),
-            "gender":fake.random_element(["M","F"]),
-            "phone_number":fake.phone_number(),
-            "email":fake.email(),
-            "address":fake.address(),
-            "postal_code":fake.postcode(),
-            "city":fake.city(),
-            "state":fake.state(),
-            "country":fake.country(),
-            "profession":fake.job(),
-            "company":fake.company(),
-            "language":fake.language_name(),
-            "marrital_status":fake.boolean(),
-            "date_generated":datetime.now().strftime("%Y-%m-%d %H:%M:%S_%f")
+            "birth_date": fake.date_of_birth().isoformat(),
+            "gender": fake.random_element(["M", "F"]),
+            "phone_number": fake.phone_number(),
+            "email": fake.email(),
+            "address": fake.address(),
+            "postal_code": fake.postcode(),
+            "city": fake.city(),
+            "state": fake.state(),
+            "country": fake.country(),
+            "profession": fake.job(),
+            "company": fake.company(),
+            "language": fake.language_name(),
+            "marrital_status": fake.boolean(),
+            "date_generated": datetime.now().strftime("%Y-%m-%d %H:%M:%S_%f")
         }
         return user
     except Exception as e:
-        print(f"Error generating sales data: {e}")
+        print(f"Error generating user data: {e}")
+
+def generate_shipping_data(transaction_ids):
+    try:
+        shipping = {
+            "shipping_id": fake.uuid4(),
+            "transaction_id": random.choice(transaction_ids), 
+            "shipping_date": fake.date_time_this_month().strftime("%Y-%m-%d %H:%M:%S"),
+            "delivery_date": fake.future_date(end_date='+30d').strftime("%Y-%m-%d"),
+            "shipping_cost": round(random.uniform(5, 50), 2),
+            "carrier": fake.company(),
+            "status": fake.random_element(["In Transit", "Delivered", "Cancelled"]),
+        }
+        return shipping
+    except Exception as e:
+        print(f"Error generating shipping data: {e}")
+
+def generate_marketing_campaign_data(user_ids):
+    try:
+        campaign = {
+            "campaign_id": fake.uuid4(),
+            "user_id": random.choice(user_ids),
+            "campaign_name": fake.bs().title(),
+            "channel": fake.random_element(["Email", "SMS", "Social Media", "Direct Mail"]),
+            "start_date": fake.date_this_year().strftime("%Y-%m-%d"),
+            "end_date": fake.future_date(end_date='+60d').strftime("%Y-%m-%d"),
+            "budget": round(random.uniform(500, 5000), 2),
+            "status": fake.random_element(["Active", "Completed", "Cancelled"]),
+        }
+        return campaign
+    except Exception as e:
+        print(f"Error generating marketing campaign data: {e}")
+
+def generate_customer_feedback_data(user_ids, transaction_ids):
+    try:
+        feedback = {
+            "feedback_id": fake.uuid4(),
+            "user_id": random.choice(user_ids),  # Relacionado com `users`
+            "transaction_id": random.choice(transaction_ids),  # Relacionado com `sales`
+            "feedback_date": fake.date_time_this_year().strftime("%Y-%m-%d %H:%M:%S"),
+            "rating": random.randint(1, 5),
+            "comments": fake.text(max_nb_chars=200),
+        }
+        return feedback
+    except Exception as e:
+        print(f"Error generating customer feedback data: {e}")
+
+def generate_product_category_data():
+    try:
+        category = {
+            "category_id": fake.uuid4(),
+            "category_name": fake.random_element(["Electronics", "Clothing", "Books", "Home Appliances", "Toys"]),
+            "description": fake.text(max_nb_chars=200),
+        }
+        return category
+    except Exception as e:
+        print(f"Error generating product category data: {e}")
+
+def generate_product_data(category_ids):
+    try:
+        product = {
+            "product_id": fake.uuid4(),
+            "category_id": random.choice(category_ids),
+            "product_name": fake.word().capitalize(),
+            "brand": fake.company(),
+            "price_per_unit": round(random.uniform(10, 1000), 2),
+        }
+        return product
+    except Exception as e:
+        print(f"Error generating product data: {e}")
+
 
 def generate_csv_file():
     temp_user_data_list = []
     temp_sales_data_list = []
-    for i in range(10000):
+    temp_shipping_data_list = []
+    temp_campaign_data_list = []
+    temp_feedback_data_list = []
+    temp_category_data_list = []
+    temp_product_data_list = []
+
+
+    # users data
+    for _ in range(1000):
         temp_user_list = [generate_user_data()]
-        temp_sales_list = [generate_sales_data()]
         temp_user_df = pd.DataFrame(temp_user_list)
-        temp_sales_df = pd.DataFrame(temp_sales_list)    
         temp_user_data_list.append(temp_user_df)
-        temp_sales_data_list.append(temp_sales_df)
+
     user_df_final = pd.concat(temp_user_data_list)
+    user_ids = user_df_final["user_id"].tolist()
+
+    # sales data
+    for _ in range(5000):
+        temp_sales_list = [generate_sales_data(user_ids)]
+        temp_sales_df = pd.DataFrame(temp_sales_list)
+        temp_sales_data_list.append(temp_sales_df)
+
     sales_df_final = pd.concat(temp_sales_data_list)
+    transaction_ids = sales_df_final["transaction_id"].tolist()
+
+    # shipping
+    for _ in range(2000):
+        temp_shipping_list = [generate_shipping_data(transaction_ids)]
+        temp_shipping_df = pd.DataFrame(temp_shipping_list)
+        temp_shipping_data_list.append(temp_shipping_df)
+
+    # marketing data
+    for _ in range(100):
+        temp_campaign_list = [generate_marketing_campaign_data(user_ids)]
+        temp_campaign_df = pd.DataFrame(temp_campaign_list)
+        temp_campaign_data_list.append(temp_campaign_df)
+
+    # feedback data
+    for _ in range(2000):
+        temp_feedback_list = [generate_customer_feedback_data(user_ids, transaction_ids)]
+        temp_feedback_df = pd.DataFrame(temp_feedback_list)
+        temp_feedback_data_list.append(temp_feedback_df)
+
+    # product category data
+    for _ in range(10):
+        temp_category_list = [generate_product_category_data()]
+        temp_category_df = pd.DataFrame(temp_category_list)
+        temp_category_data_list.append(temp_category_df)
+
+    category_df_final = pd.concat(temp_category_data_list)
+    category_ids = category_df_final["category_id"].tolist()
+
+    # product data
+    for _ in range(200):
+        temp_product_list = [generate_product_data(category_ids)]
+        temp_product_df = pd.DataFrame(temp_product_list)
+        temp_product_data_list.append(temp_product_df)
+
+    product_df_final = pd.concat(temp_product_data_list)
+
     date = datetime.now().strftime("%Y-%m-%d %H:%M:%S_%f")
-    path_sales = os.path.join("data/sales",f"sales_{date}.csv")
-    path_users = os.path.join("data/users",f"users_{date}.csv")
-    sales_df_final.to_csv(path_sales,sep=",",index=False,encoding="utf-8")
-    user_df_final.to_csv(path_users,sep=",",index=False,encoding="utf-8")
+    user_df_final.to_csv(f"data/users/users_{date}.csv", index=False, encoding="utf-8")
+    sales_df_final.to_csv(f"data/sales/sales_{date}.csv", index=False, encoding="utf-8")
+    pd.concat(temp_shipping_data_list).to_csv(f"data/shipping/shipping_{date}.csv", index=False, encoding="utf-8")
+    pd.concat(temp_campaign_data_list).to_csv(f"data/marketing_campaigns/campaigns_{date}.csv", index=False, encoding="utf-8")
+    pd.concat(temp_feedback_data_list).to_csv(f"data/feedback/feedback_{date}.csv", index=False, encoding="utf-8")
+    category_df_final.to_csv(f"data/product_categories/categories_{date}.csv", index=False, encoding="utf-8")
+    product_df_final.to_csv(f"data/products/products_{date}.csv", index=False, encoding="utf-8")
 
     
 
 if __name__ == '__main__':
-        while True:
-            generate_csv_file()
-            time.sleep(0.2)
+    generate_csv_file()
+
  
 
     
